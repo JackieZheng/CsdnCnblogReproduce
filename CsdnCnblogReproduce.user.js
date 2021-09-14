f0982e3a9c41d334397b95f2cdb5c90143304c87
JackieZheng <zhengxinzhe@admans.net>
2021-09-14 22:23:05 +0800

    Update CsdnCnblogReproduce.user.js
    本版本为4.0测试版，可能很多地方有问题。

--- CsdnCnblogReproduce.user.js
+++ CsdnCnblogReproduce.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
-// @name         CSDN,CNBLOG博客文章一键转载插件 
-// @version      3.10
+// @name         CSDN,CNBLOG博客文章一键转载插件
+// @version      4.0.1
 // @description  CSDN博客文章转载插件 可以实现CSDN上的文章一键转载
 // @author       By Jackie http://csdn.admans.cn/
 // @match        *://blog.csdn.net/*/article/details/*
@@ -10,9 +10,12 @@
 // @match        *://www.cnblogs.com/*/p/*.html
 // @match        *://www.cnblogs.com/*/articles/*.html
 // @match        *://www.cnblogs.com/*/archive/*/*/*/*.html
+// @match        *://*.blog.csdn.net/article/details/*
 // @match        *://i.cnblogs.com/EditArticles.aspx?opt=1
 // @match        *://i.cnblogs.com/EditPosts.aspx?opt=1
 // @match        *://i-beta.cnblogs.com/posts/edit?opt=1
+// @match        *://mp.csdn.net/mp_blog/creation/editor?opt=1
+// @match        *://editor.csdn.net/md/?not_checkout=1
 // @require      https://unpkg.com/turndown/dist/turndown.js
 // @grant    GM_addStyle
 // @namespace https://greasyfork.org/users/164689
@@ -20,7 +23,7 @@
 // ==/UserScript==
 GM_addStyle("#ReproduceBtn{position: absolute;float: right;right: 0px;width: auto;background: #0f962191;z-index: 9999;color: white;text-align: center;margin: 5px;padding: 5px;border-radius: 5px;cursor: pointer;line-height: 100%;}"); (function() {
     'use strict';
-    //document.domain="csdn.net";  
+    //document.domain="csdn.net";
     var cnblog = location.href.indexOf("cnblogs.com") > -1 ? true: false;
     if (cnblog) {
         document.domain = "cnblogs.com";
@@ -43,12 +46,14 @@ GM_addStyle("#ReproduceBtn{position: absolute;float: right;right: 0px;width: aut
         divBtn.onclick = function() {
             window.open(posteditUrl);
         }
-    } else //文章发布窗口
+    }
+    //文章发布窗口
+    else
     {
         document.onreadystatechange = function(e) {
             if (document.readyState == 'complete') {
                 setTimeout(function() {
-                    if (window.opener && location.href.indexOf("?opt=1") > -1) {
+                    if (window.opener && (location.href.indexOf("?opt=1") > -1||location.href.indexOf("?not_checkout=1") > -1)) {
                         var contentDocumentbody = document.getElementsByTagName("iframe")[0] == undefined ? null: document.getElementsByTagName("iframe")[0].contentDocument.body;
                         var authorName = "";
                         if (window.opener.document.getElementsByClassName('follow-nickName').length > 0) {
@@ -59,7 +64,7 @@ GM_addStyle("#ReproduceBtn{position: absolute;float: right;right: 0px;width: aut
                             authorName = window.opener.document.getElementById('author_profile_detail').childNodes[1].innerText;
                         }
                         var blogContent = (window.opener.document.getElementById('content_views') || window.opener.document.getElementById('cnblogs_post_body')).innerHTML + "<br>---------------------" + "<br>作者：" + authorName + "<br>来源：" + (cnblog == true ? "CNBLOGS": "CSDN") + "<br>原文：" + window.opener.location.href + "<br>版权声明：本文为作者原创文章，转载请附上博文链接！" + "<br>内容解析By：<a href=https://greasyfork.org/zh-CN/scripts/381053-csdn-cnblog%E5%8D%9A%E5%AE%A2%E6%96%87%E7%AB%A0%E4%B8%80%E9%94%AE%E8%BD%AC%E8%BD%BD%E6%8F%92%E4%BB%B6 target=_blank>CSDN,CNBLOG博客文章一键转载插件</a>";
-                        var input_title = (document.getElementById('txtTitle') || document.getElementById('Editor_Edit_txbTitle') || document.querySelector('input.cnb-input'));
+                        var input_title = (document.getElementById('txtTitle') || document.getElementById('Editor_Edit_txbTitle') || document.querySelector('input.cnb-input'))||document.getElementsByClassName("article-bar__title")[0];
                         if (input_title) {
                             var aTitle = "[转]" + window.opener.document.title.split('_')[0];
                             aTitle = aTitle + "(转载请删除括号里的内容)";
@@ -81,8 +86,7 @@ GM_addStyle("#ReproduceBtn{position: absolute;float: right;right: 0px;width: aut
 
                                         var preText = '';
                                         var codeTag = window.opener.document.getElementsByTagName('pre')[i];
-                                        
-                                        if(codeTag){  
+                                        if(codeTag){
                                             if(codeTag.querySelector("ul[class*='pre-numbering']")){
                                              codeTag.querySelector("ul[class*='pre-numbering']").remove();
                                             }
@@ -98,49 +102,46 @@ GM_addStyle("#ReproduceBtn{position: absolute;float: right;right: 0px;width: aut
                                         var preCodeHtml = "<pre><code class=\"hljs\">" + preText.replace(/</g, '&lt;').replace(/>/g, '&gt;') + "</code></pre>";
 
                                         aContent = aContent.replace(arrMactches[i], preCodeHtml);
-                                        
-
                                     }
 
                                 }
-                             
-                             
-                             
                             }
                             contentDocumentbody.innerHTML = aContent;
                             if (contentDocumentbody.children.ReadBtn) contentDocumentbody.children.ReadBtn.remove();
                             if (contentDocumentbody.children.ReproduceBtn) contentDocumentbody.children.ReproduceBtn.remove();
-                            let mathspace=contentDocumentbody.querySelectorAll("[width*='thickmathspace']");                              
+                            let mathspace=contentDocumentbody.querySelectorAll("[width*='thickmathspace']");
                               mathspace.forEach(function(ele){
                                ele.outerHTML=" ";
                               });
-                            let mathspan=contentDocumentbody.querySelectorAll("[class*='MathJax']");                              
+                            let mathspan=contentDocumentbody.querySelectorAll("[class*='MathJax']");
                             mathspan.forEach(function(ele){
                                let innerText= ele.innerText;
                                ele.outerHTML="<span>"+innerText+"</span>";
                               });
-                            
                         }
-                        //if(document.getElementById("selType"))document.getElementById("selType").value="2";
-                         if (document.getElementsByClassName("textfield")) document.getElementsByClassName("textfield")[0].options[2].selected = true;
-                         if (document.querySelector("[class^='ipt-box']")) document.querySelector("[class^='ipt-box']").querySelector("[class^='el-input__inner']").value = window.opener.location.href.split('?')[0];
+                        if (document.querySelector("[class^='el-input__inner']")) document.querySelector("[class^='el-input__inner']").value = window.opener.location.href.split('?')[0];
                          if (document.querySelector("[class^='el-checkbox__original']")) document.querySelector("[class^='el-checkbox__original']").checked = true;
-                         if(document.querySelectorAll("[class*='copyright-box']"))
+                         if(document.querySelectorAll("[class*='copyright-box']").length>0)
                          {
                            document.querySelectorAll("[class*='copyright-box']")[0].style.display="";
                            document.querySelectorAll("[class*='copyright-box']")[1].style.display="";
                          }
 
+                        var htmlContent = blogContent.replace(/<ul class=\"pre-numbering\"[\s\S].*<\/ul>/g, '');
+                        htmlContent = htmlContent.replace(/<div class=\"cnblogs_code_toolbar\"[\s\S].*?<\/div>/g, '');
+                        htmlContent = htmlContent.replace(/<a[\s\S].*class=\"toolbar_item[\s\S].*>?<\/a>/g, '');
+                        htmlContent = htmlContent.replace(/<div id=\"ReproduceBtn\"[\s\S].*?<\/div>/g, '');
+                        htmlContent = htmlContent.replace(/<div id=\"ReadBtn\"[\s\S].*?<\/div>/g, '');
+                        htmlContent = htmlContent.replace(/<div class=\"line[\s\S].*>\d+?<\/div>/g, '');
+                        var turndownService = new TurndownService();
+                        var mdContent = turndownService.turndown(htmlContent);
+console.log(mdContent);
+                        var csdnMDeditor=document.getElementsByClassName("editor__inner");
+                        if (csdnMDeditor) {
+                            csdnMDeditor[0].innerHTML=mdContent;
+                        }
                         var cnblogsMDeditor = document.getElementById("Editor_Edit_EditorBody");
                         if (cnblogsMDeditor) {
-                            var htmlContent = blogContent.replace(/<ul class=\"pre-numbering\"[\s\S].*<\/ul>/g, '');
-                            htmlContent = htmlContent.replace(/<div class=\"cnblogs_code_toolbar\"[\s\S].*?<\/div>/g, '');
-                            htmlContent = htmlContent.replace(/<a[\s\S].*class=\"toolbar_item[\s\S].*>?<\/a>/g, '');
-                            htmlContent = htmlContent.replace(/<div id=\"ReproduceBtn\"[\s\S].*?<\/div>/g, '');
-                            htmlContent = htmlContent.replace(/<div id=\"ReadBtn\"[\s\S].*?<\/div>/g, '');
-                            htmlContent = htmlContent.replace(/<div class=\"line[\s\S].*>\d+?<\/div>/g, '');
-                            var turndownService = new TurndownService();
-                            var mdContent = turndownService.turndown(htmlContent);
                             cnblogsMDeditor.value = mdContent;
                         } else {
                             if (input_title) {
